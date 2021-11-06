bcf3e043450648ff20c792aac6d2f6efa6b8825c
Almaz1984 <57220658+Almaz1984@users.noreply.github.com>
2021-11-06 15:05:58 +0500

    some changes

--- app/src/main/java/com/almaz/poem_cpc5/MainActivity.kt
+++ app/src/main/java/com/almaz/poem_cpc5/MainActivity.kt
@@ -3,60 +3,65 @@ package com.almaz.poem_cpc5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.PackageManagerCompat
 import kotlin.collections.List as List
 import kotlin.collections.mutableListOf as mutableListOf
+import androidx.core.content.PackageManagerCompat.LOG_TAG
+import androidx.core.content.PackageManagerCompat.LOG_TAG
 
 private const val MY_TAG="MyTag"
 class MainActivity : AppCompatActivity() {
     private val poem: MutableList<String> = mutableListOf()
-    private var i=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        poem.add("Ты видел деву на скале")
-        poem.add("В одежде белой над волнами")
-        poem.add("Когда, бушуя в бурной мгле,")
-        poem.add("Играло море с берегами,")
-        poem.add("Когда луч молний озарял")
-        poem.add("Ее всечасно блеском алым")
-        poem.add("И ветер бился и летал")
-        poem.add("С ее летучим покрывалом?")
-        poem.add("Прекрасно море в бурной мгле")
-        poem.add("И небо в блесках без лазури;")
-        poem.add("Но верь мне: дева на скале")
-        poem.add("Прекрасней волн, небес и бури.")
-        sendLog()
+        Log.d(MY_TAG, "onCreate")
     }
 
     override fun onStart() {
         super.onStart()
-        sendLog()
+        Log.d(MY_TAG, "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        sendLog()
+        Log.d(MY_TAG, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        sendLog()
+        Log.d(MY_TAG, "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        sendLog()
+        Log.d(MY_TAG, "onStop")
     }
 
     override fun onRestart() {
         super.onRestart()
-        sendLog()
+        Log.d(MY_TAG, "onRestart")
     }
 
-    fun sendLog(){
-        Log.d(MY_TAG, poem[i])
-        Log.d(MY_TAG, poem[i+1])
-        i += 2
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MY_TAG, "onDestroy")
     }
+
+
+ //   override fun onSaveInstanceState(outState: Bundle) {
+  //      super.onSaveInstanceState(outState)
+  //      Log.d(MY_TAG, "onSaveInstanceState")
+  //  }
+
+   // override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+  //      super.onRestoreInstanceState(savedInstanceState)
+  //     Log.d(MY_TAG, "onRestoreInstanceState")
+  //  }
+
+  //  private fun sendLog(i:Int): Unit{
+  //      Log.d(MY_TAG, poem[i*2])
+  //      Log.d(MY_TAG, poem[i*2+1])
+  //  }
 
 }
